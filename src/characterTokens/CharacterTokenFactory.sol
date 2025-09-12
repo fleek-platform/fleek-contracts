9e9102fceea54075cb22ebebc9675cfd19348fe7
mulf <mail@mulf.wtf>
2025-09-12 15:29:42 -0500

    stub factory actions

--- src/characterTokens/CharacterTokenFactory.sol
+++ src/characterTokens/CharacterTokenFactory.sol
@@ -5,6 +5,13 @@ import { BaseCharacterToken } from "./BaseToken.sol";
 
 contract CharacterTokenFactory {
     function deployNewCharacterToken(string memory name, string memory symbol) external {
-        new BaseCharacterToken(msg.sender, name, symbol);
+        BaseCharacterToken newToken = new BaseCharacterToken(address(this), name, symbol);
+        newToken.approve(address(this), 1_000_000_000e18);
+
+        // TODO: add sablier lockup
+
+        // TODO: deploy LP with custom bonding curve hook
+
+        // TODO: transfer remaining tokens to treasury
     }
 }
