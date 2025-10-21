cbf18f03ce8df9c83d89f65113e7773cdec1abbd
mulf <mail@mulf.wtf>
2025-10-21 17:53:15 -0500

    pass in supply from factory to help with vaildation

--- src/creator-tokens/CreatorCoin.sol
+++ src/creator-tokens/CreatorCoin.sol
@@ -5,14 +5,14 @@ import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { ERC20Permit } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import { ERC20Burnable } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-/// @title Creator Token
+/// @title Creator Coin
 /// @notice Implementation ERC20 token with permit and burn
-/// @dev Fixed supply of 1B tokens
+/// @dev Fixed supply of 1 million tokens
 contract CreatorCoin is ERC20, ERC20Permit, ERC20Burnable {
-    constructor(string memory _name, string memory _symbol)
+    constructor(string memory _name, string memory _symbol, uint256 supply)
         ERC20(_name, _symbol)
         ERC20Permit(_name)
     {
-        _mint(msg.sender, 1_000_000e18);
+        _mint(msg.sender, supply);
     }
 }
