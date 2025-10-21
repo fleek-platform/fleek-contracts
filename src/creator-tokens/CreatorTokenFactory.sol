4a6dbc95d97e03fa8a37f62b6d9bdb2d481c73f4
mulf <mail@mulf.wtf>
2025-10-21 17:06:23 -0500

    update factory with new tokenomics

--- src/creator-tokens/CreatorTokenFactory.sol
+++ src/creator-tokens/CreatorTokenFactory.sol
@@ -9,11 +9,26 @@ import {
 } from "@openzeppelin/contracts/access/extensions/AccessControlDefaultAdminRules.sol";
 
 contract CharacterTokenFactory is AccessControlDefaultAdminRules {
-    address FOUNDATION_MULTISIG = 0x5719061AD5052C1f2E4c942c68F35935adD31f7E;
     address constant FLK = 0xE0969ec84456b7e4d3Dd2181fB5265EDbB63F7BD;
-    uint256 constant GRADUATION_THRESHOLD = 20_000e18; // 20,000 FLK
-    uint256 constant BASE_PRICE = 8e13; //  calibrated base price
-    uint256 constant CHARACTER_SUPPLY = 450_000_000e18;
+
+    address FOUNDATION_MULTISIG = 0x5719061AD5052C1f2E4c942c68F35935adD31f7E;
+    address fanPoolWallet = 0x491193C8C2BA55503dBf83eB608E69E93b9Ba96a;
+
+    uint256 graduationThreshold = 20_675e18; // Amount in FLK
+    uint256 basePrice = 1e15;
+
+    uint256 bondingCurveAllocation = 450_000e18;
+    uint256 creatorAllocation = 250_000e18;
+    uint256 creatorFundAllocation = 50_000e18;
+    uint256 fanPoolAllocation = 250_000e18;
+
+    event TokenDeployed(address newToken, address bondingCurve);
+    event GraduationThresholdUpdated(uint256 newThreshold);
+    event BondingCurveAllocationUpdated(uint256 newAllocation);
+    event BasePriceUpdated(uint256 newBasePrice);
+    event CreatorFundAllocationUpdated(uint256 newAllocation);
+    event FanPoolWalletUpdated(address newWallet);
+    event FanPoolAllocationUpdated(uint256 newAllocation);
 
     constructor() AccessControlDefaultAdminRules(3 days, FOUNDATION_MULTISIG) { }
 
@@ -37,13 +52,52 @@ contract CharacterTokenFactory is AccessControlDefaultAdminRules {
             _creator,
             FLK,
             address(creatorCoin),
-            GRADUATION_THRESHOLD,
-            BASE_PRICE,
-            CHARACTER_SUPPLY / 2
+            graduationThreshold,
+            basePrice,
+            bondingCurveAllocation / 2
         );
 
-        creatorCoin.transfer(FOUNDATION_MULTISIG, 50_000_000e18);
-        creatorCoin.transfer(address(bondingCurve), CHARACTER_SUPPLY);
-        creatorCoin.transfer(address(creatorVesting), 250_000_000e18);
+        creatorCoin.transfer(FOUNDATION_MULTISIG, creatorFundAllocation);
+        creatorCoin.transfer(address(bondingCurve), bondingCurveAllocation);
+        creatorCoin.transfer(address(creatorVesting), creatorAllocation);
+        creatorCoin.transfer(fanPoolWallet, fanPoolAllocation);
+
+        emit TokenDeployed({ newToken: address(creatorCoin), bondingCurve: address(bondingCurve) });
+    }
+
+    function updateGraduationThreshold(uint256 newThreshold) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        graduationThreshold = newThreshold;
+        emit GraduationThresholdUpdated({ newThreshold: newThreshold });
+    }
+
+    function updateBondingCurveAllocation(uint256 newAllocation)
+        external
+        onlyRole(DEFAULT_ADMIN_ROLE)
+    {
+        bondingCurveAllocation = newAllocation;
+        emit BondingCurveAllocationUpdated({ newAllocation: newAllocation });
+    }
+
+    function updateBasePrice(uint256 newBasePrice) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        basePrice = newBasePrice;
+        emit BasePriceUpdated({ newBasePrice: newBasePrice });
+    }
+
+    function updateCreatorFundAllocation(uint256 newAllocation)
+        external
+        onlyRole(DEFAULT_ADMIN_ROLE)
+    {
+        creatorFundAllocation = newAllocation;
+        emit CreatorFundAllocationUpdated({ newAllocation: newAllocation });
+    }
+
+    function updateFanPoolWallet(address newWallet) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        fanPoolWallet = newWallet;
+        emit FanPoolWalletUpdated({ newWallet: newWallet });
+    }
+
+    function updateFanPoolAllocation(uint256 newAllocation) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        fanPoolAllocation = newAllocation;
+        emit FanPoolAllocationUpdated({ newAllocation: newAllocation });
     }
 }
